45a3cfc17e3d3b88ee88b63972745a8547411fb6
Madeline Kusters <mkusters@amazon.com>
2022-06-14 22:11:16 -0700

    chore: add dependencies on yaml and aws-sdk

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,10 @@ const project = new javascript.NodeProject({
   defaultReleaseBranch: "main",
   name: "awscdk-v1-stack-finder",
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: [
+    "aws-sdk",
+    "yaml",
+  ],
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
